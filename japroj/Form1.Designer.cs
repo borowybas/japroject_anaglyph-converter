42a70ba2831cfa72b5996d861b5a0134722f41be
borowybas <bar.borowy@gmail.com>
2023-01-15 00:29:19 +0100

    Add project files.

---
+++ japroj/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace japroj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chooseFileButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::japroj.Properties.Resources.luluv21;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chooseFileButton
+            // 
+            this.chooseFileButton.Location = new System.Drawing.Point(314, 72);
+            this.chooseFileButton.Name = "chooseFileButton";
+            this.chooseFileButton.Size = new System.Drawing.Size(147, 41);
+            this.chooseFileButton.TabIndex = 1;
+            this.chooseFileButton.Text = "Choose image";
+            this.chooseFileButton.UseVisualStyleBackColor = true;
+            this.chooseFileButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(979, 72);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(128, 43);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(772, 142);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1430, 756);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.chooseFileButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button chooseFileButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ japroj/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace japroj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String imageLocation = "";
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "jpg files(.*jpg)|*.jpg| PNG files(.*png)|*.png| All Files(*.*)|*.*";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    imageLocation = dialog.FileName;
+                    pictureBox1.ImageLocation = imageLocation;
+
+                    System.Drawing.Image imag = System.Drawing.Image.FromFile(imageLocation);
+                    imag.Save(@"C:\Users\Admin\source\repos\japroj\image.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                }
+            }
+            catch (Exception) {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+        Bitmap originalBitmap;
+        Bitmap rightBitmap;//cyjan
+        Bitmap leftBitmap;//red
+        Bitmap resultBitmap;
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //retrieve the image
+                originalBitmap = new Bitmap(@"C:\Users\Admin\source\repos\japroj\image.bmp", true);
+                rightBitmap = new Bitmap(@"C:\Users\Admin\source\repos\japroj\image.bmp", true);
+                leftBitmap = new Bitmap(@"C:\Users\Admin\source\repos\japroj\image.bmp", true);
+                resultBitmap = new Bitmap(@"C:\Users\Admin\source\repos\japroj\image.bmp", true);
+
+
+                int x, y;
+
+                //loop through the images pixels to reset color
+                for(x=0; x< originalBitmap.Width; x++)
+                {
+                    for(y=0; y< originalBitmap.Height; y++)
+                    {
+                        Color pixelColor = originalBitmap.GetPixel(x, y);
+                        Color leftColor = Color.FromArgb(pixelColor.R, 0, 0);
+                        Color rightColor = Color.FromArgb(0, pixelColor.G, pixelColor.B);
+
+                        leftBitmap.SetPixel(x, y, leftColor);
+
+                        if (x < 5)
+                             rightBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                            
+                        if((x+5) < originalBitmap.Width)
+                            rightBitmap.SetPixel(x+5, y, rightColor);
+
+                        resultBitmap.SetPixel(x, y, Color.FromArgb(leftBitmap.GetPixel(x, y).R, rightBitmap.GetPixel(x, y).G, rightBitmap.GetPixel(x, y).B));
+                    }
+
+                }
+
+                pictureBox2.Image = resultBitmap;
+
+            }
+            catch (ArgumentException) 
+            {
+                MessageBox.Show("There was an error." +
+                "Check the path to the image file.");
+            }
+        }
+
+
+    }
+}
