f94d6773677d4405d2f7f882a44b338de116df95
borowybas <bar.borowy@gmail.com>
2023-01-24 20:17:03 +0100

    load stereopair images instead of one, conversion algorithm now in c++ dll

--- japroj/Form1.Designer.cs
+++ japroj/Form1.Designer.cs
@@ -45,6 +45,7 @@ namespace japroj
             this.cConversionTime = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -67,7 +68,7 @@ namespace japroj
             this.chooseFileButton.Name = "chooseFileButton";
             this.chooseFileButton.Size = new System.Drawing.Size(147, 41);
             this.chooseFileButton.TabIndex = 1;
-            this.chooseFileButton.Text = "Choose image";
+            this.chooseFileButton.Text = "Choose left image";
             this.chooseFileButton.UseVisualStyleBackColor = true;
             this.chooseFileButton.Click += new System.EventHandler(this.chooseFileButton_Click);
             // 
@@ -205,11 +206,22 @@ namespace japroj
             this.label2.TabIndex = 0;
             this.label2.Text = "C++: ";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(51, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 41);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Choose right image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1398, 759);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
@@ -250,6 +262,7 @@ namespace japroj
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label asmConvesionTime;
         private System.Windows.Forms.Label cConversionTime;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- japroj/Form1.cs
+++ japroj/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace japroj
 {
@@ -15,7 +16,7 @@ namespace japroj
     {
         
         [DllImport(@"C:\Users\Admin\source\repos\japroj\x64\Debug\DllC.dll")]
-        public static extern int addNumbers(int a, int b);
+        public static extern void convertCPP(byte[] leftRgbValues, byte[] rightRgbValues, int bytesLength);
 
         [DllImport(@"C:\Users\Admin\source\repos\japroj\x64\Debug\DllAsm.dll")]
         static extern int MyProc1(int a, int b);
@@ -35,7 +36,7 @@ namespace japroj
                 if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
                     pictureBox1.ImageLocation = dialog.FileName;
-                    originalBitmap = new Bitmap(dialog.FileName);//dynamic cast to do
+                    leftBitmap = new Bitmap(dialog.FileName);//dynamic cast to do
 
                     if (radioButton1.Checked || radioButton2.Checked)
                         convertButton.Enabled = true;
@@ -45,12 +46,34 @@ namespace japroj
                 MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.tif;...";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    //pictureBox1.ImageLocation = dialog.FileName;
+                    rightBitmap = new Bitmap(dialog.FileName);//dynamic cast to do
+
+                    if (radioButton1.Checked || radioButton2.Checked)
+                        convertButton.Enabled = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An Error Occured", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
 
         Bitmap originalBitmap;
         Bitmap rightBitmap;//cyjan
         Bitmap leftBitmap;//red
         Bitmap resultBitmap;
+        bool asmConvert = false;
         private void convertButton_Click(object sender, EventArgs e)
         {
             // Display the ProgressBar control.
@@ -64,6 +87,7 @@ namespace japroj
 
             try
             {
+                /*
                 //retrieve the image
                 rightBitmap = new Bitmap(originalBitmap);
                 leftBitmap = new Bitmap(originalBitmap);
@@ -86,18 +110,20 @@ namespace japroj
                         leftBitmap.SetPixel(x, y, leftColor);
 
                         if (x < 20)
-                             rightBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                            rightBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
                             
                         if((x+20) < originalBitmap.Width)
                             rightBitmap.SetPixel(x+20, y, rightColor);
 
                         resultBitmap.SetPixel(x, y, Color.FromArgb(leftBitmap.GetPixel(x, y).R, rightBitmap.GetPixel(x, y).G, rightBitmap.GetPixel(x, y).B));
                         progressBar1.PerformStep();
+                        //IntPtr f;
                     }
 
                 }
-
                 pictureBox2.Image = resultBitmap;
+                */
+                lockUnlockBits();
 
             }
             catch (ArgumentException) 
@@ -105,6 +131,93 @@ namespace japroj
                 MessageBox.Show("There was an error." +
                 "Check the path to the image file.");
             }
+        }
+
+        private void lockUnlockBits()
+        {
+            // Create a new bitmap.
+            //Bitmap bmp = new Bitmap("c:\\fakePhoto.jpg");
+            //originalBitmap
+
+            // Lock the bitmap's bits.  
+            //Bitmap resultBmp = new Bitmap(originalBitmap.Width+20, originalBitmap.Height);
+            Rectangle leftRect = new Rectangle(0, 0, leftBitmap.Width, leftBitmap.Height);
+            Rectangle rightRect = new Rectangle(0, 0, rightBitmap.Width, rightBitmap.Height);
+            
+
+            System.Drawing.Imaging.BitmapData leftBmpData =
+                leftBitmap.LockBits(leftRect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                leftBitmap.PixelFormat);
+
+            System.Drawing.Imaging.BitmapData rightBmpData =
+               rightBitmap.LockBits(rightRect, System.Drawing.Imaging.ImageLockMode.WriteOnly,
+               rightBitmap.PixelFormat);
+
+            // Get the address of the first line.
+            IntPtr leftPtr = leftBmpData.Scan0;
+            IntPtr righttPtr = rightBmpData.Scan0;
+
+
+            // Declare an array to hold the bytes of the bitmap.
+            int leftBytes = Math.Abs(leftBmpData.Stride) * leftBitmap.Height;
+            byte[] leftRgbValues = new byte[leftBytes];
+            int rightBytes = Math.Abs(rightBmpData.Stride) * rightBitmap.Height;
+            byte[] rightRgbValues = new byte[rightBytes];
+
+
+            // Copy the RGB values into the array.
+            System.Runtime.InteropServices.Marshal.Copy(leftPtr, leftRgbValues, 0, leftBytes);
+            System.Runtime.InteropServices.Marshal.Copy(righttPtr, rightRgbValues, 0, rightBytes);
+
+
+            // Set every third value to 255. A 24bpp bitmap will look red.  
+
+            //insert convering functions here
+            // (asmConvert == true) ? convertAssembly() : convertCPP(); 
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            if (asmConvert == true)
+                convertAssembly();
+            else convertCPP(leftRgbValues, rightRgbValues, leftRgbValues.Length);
+            stopwatch.Stop();
+
+            cConversionTime.Text = stopwatch.ElapsedMilliseconds.ToString()+" ms";
+            // Copy the RGB values back to the bitmap
+            System.Runtime.InteropServices.Marshal.Copy(leftRgbValues, 0, leftPtr, leftBytes);
+            System.Runtime.InteropServices.Marshal.Copy(rightRgbValues, 0, righttPtr, rightBytes);
+
+
+            // Unlock the bits.
+            leftBitmap.UnlockBits(leftBmpData);
+            rightBitmap.UnlockBits(rightBmpData);
+
+            // Draw the modified image.
+            //e.Graphics.DrawImage(originalBitmap, 0, 150);
+            pictureBox2.Image = rightBitmap;
+
+        }
+
+        private void convertCP(byte[] leftRgbValues, byte[] rightRgbValues)
+        {
+            
+
+            for (int counter = 0; counter < leftRgbValues.Length; counter += 3)
+            {//BGR
+
+
+                rightRgbValues[counter] = rightRgbValues[counter];
+                rightRgbValues[counter + 1] = rightRgbValues[counter + 1];
+                rightRgbValues[counter + 2] = leftRgbValues[counter + 2];
+
+            }
+
+
+        }
+
+        private void convertAssembly()
+        {
+
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -124,16 +237,16 @@ namespace japroj
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             if (pictureBox1.ImageLocation != null)
-                convertButton.Enabled = true;
+                convertButton.Enabled = true; asmConvert = true;
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             if (pictureBox1.ImageLocation != null)
-                convertButton.Enabled = true;
+                convertButton.Enabled = true; asmConvert = false;
 
         }
 
-       
+        
     }
 }
